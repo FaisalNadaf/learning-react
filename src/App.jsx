be4463a8b6908c984c4f799b3271d620b05334d8
faisal <faisalfnadaf@gmail.com>
2024-02-04 10:59:01 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import './App.css';
-import MyDetails from './myDetails';
+import "./App.css";
+import MyDetails from "./myDetails";
 
 function App() {
-  return  <MyDetails/>;
+  return (
+    <MyDetails
+      name={"sam"}
+      college={"uk univercity"}
+      sem={"lastt"}
+      learning={"football"}
+      skill={"fast-running"}
+      focus={"GOALLL"}
+    />
+  );
 }
-
 export default App;
--- src/myDetails.jsx
+++ src/myDetails.jsx
@@ -1,15 +1,24 @@
-function MyDetails() {
-    return (
-      <>
-        <h1>My name is Faisal</h1>
-        <h2>From KLE RLSI BCA</h2>
-        <h3>First semester</h3>
-        <h4>Learning React</h4>
-        <h5>Love coding</h5>
-        <h6>Just code</h6>
-      </>
-    );
-  }
-  
-  export default MyDetails;
-  
+import "./myDetails.css";
+
+function MyDetails({
+  name = "NULL",
+  college = "NULL",
+  sem = "NULL",
+  learning = "NULL",
+  skill = "NULL",
+  focus = "NULL",
+}) {
+  return (
+    <>
+    <p>{name}, a {sem}-semester student at {college} College, is currently immersed in learning {learning}. His primary focus is on {focus}, where he hones his skills. {name}'s dedication to mastering {learning} aligns with his broader goal of becoming proficient in {learning}, showcasing his commitment to advancing his {skill} abilities during his {learning} journey.</p>
+      {/* <p>NAME :{name}</p>
+      <p>COLLEGE :{college}</p>
+      <p>SEMESTER :{sem}</p>
+      <p>LEARNING :{learning}</p>
+      <p>SKILL :{skill}</p>
+      <p>FOCUS :{focus}</p> */}
+    </>
+  );
+}
+
+export default MyDetails;
