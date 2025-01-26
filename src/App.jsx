0f597fb6f4b75bcf0c4ca07f5e715ed9d9108f7a
Faisal <faisalfnadaf@gmail.com>
2025-01-26 23:37:05 +0530

    add Todolist component and update App layout; comment out Cards component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import Cards from "./card/Cards";
 import Comment from "./comment";
 import Liked from "./likeBtn";
 import LudoBoard from "./ludoBoard";
+import Todolist from "./todaList_o1/todolist";
 
 function App() {
   return (
     <>
-      <div className="flex h-full w-full ">
-      
-        <Cards />
-      </div>
-
+      <Todolist />
+      {/* <div className="flex h-full w-full "><Cards /></div> */}
       {/* <ProductTab/> */}
       {/* <LudoBoard/> */}
       {/* <Comment/> */}
---
+++ src/todaList_o1/todolist.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Todolist = () => {
+  return <div className="w-full h-screen ">
+    <div></div>
+  </div>;
+};
+
+export default Todolist;
